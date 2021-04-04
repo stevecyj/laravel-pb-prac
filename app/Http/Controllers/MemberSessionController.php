f2a232ffaff162b60937c2c0555f551254675444
root <root@localhost.localdomain>
2021-04-04 17:42:04 +0800

    about session, to be continued

--- app/Http/Controllers/MemberSessionController.php
+++ app/Http/Controllers/MemberSessionController.php
@@ -9,7 +9,40 @@ class MemberSessionController extends Controller
 {
     public function create()
     {
-        return view('members.logIn');
+        // $value = session('test_key','default'); //預設值
+        // session(["test_key"=>"new value",
+        //         "test_key1" => "new value2"
+        //         ]); // 儲存 new session
+        // var_dump($value);
+        // dd($value);
+
+        // 所有 session
+        // dd(session()->all());
+
+        // has()
+        // if(session()->has('test_key')){
+        //     dd(session()->all());
+        // }
+
+        /*
+        |--------------------------------------------------------------------------
+        | array
+        |--------------------------------------------------------------------------
+        |
+        */
+
+        var_dump(session('user.teams'));
+        echo "<hr/>";
+
+        session()->put('user.teams',[]);
+        var_dump(session('user.teams'));
+        echo "<hr/>";
+
+        session()->push('user.teams','developers');
+        var_dump(session('user.teams'));
+        echo "<hr/>";
+
+        // return view('members.logIn');
     }
 
     public function store(Request $request)
@@ -19,7 +52,8 @@ class MemberSessionController extends Controller
             'password' => $request->password,
         ])->first();
 
-        var_dump($member);
+        // var_dump($member);
+        dd($member);
         // return redirect('/');
     }
 
